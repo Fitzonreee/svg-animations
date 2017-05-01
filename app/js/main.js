571b501c2348ee22746fad456e7e4d8495265a97
Kevin Fitzhenry <kevinfitzhenry@iMac.local>
2017-05-01 15:32:43 -0700

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#liquid-stain-fighter-bright-booster').load('svgs/liquid-stain-fighter-bright-booster.html');
+  $('#purex-hdd-fresh-bluebonnet').load('svgs/purex-hdd-fresh-bluebonnet.html');
+  $('#baby').load('svgs/baby.html');
+  $('#liquid-starch').load('svgs/liquid-starch.html');
+  $('#original-fresh').load('svgs/original-fresh.html');
+  $('#sunny-linen').load('svgs/sunny-linen.html');
+  $('#almond-aloe').load('svgs/almond-aloe.html');
+  $('#hdd-lavender-blossom').load('svgs/hdd-lavender-blossom.html');
+  $('#felsnaptha').load('svgs/felsnaptha.html');
+  $('#crystals-well-being').load('svgs/crystals-well-being.html');
+  $('#oxi-stain-remover').load('svgs/oxi-stain-remover.html');
+  $('#tropical-splash').load('svgs/tropical-splash.html');
+  $('#tropical-escape').load('svgs/tropical-escape.html');
+  $('#crystals-tahitian-breeze').load('svgs/crystals-tahitian-breeze.html');
+  $('#crystals-fresh-spring-waters').load('svgs/crystals-fresh-spring-waters.html');
+  $('#hdd-fresh-spring-waters').load('svgs/hdd-fresh-spring-waters.html');
+});
